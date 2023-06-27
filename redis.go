5bc56c85d3d6e9ff5f9806c82610b1a10f0773cc
Mehmed Bazdar <mehmedbazdaret@gmail.com>
2023-06-27 21:39:44 +0200

    Implement rate limiter for ssh handler;

--- redis.go
+++ redis.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/redis/go-redis/v9"
 	"time"
 )
@@ -18,14 +19,25 @@ func (r *RedisStore) Set(ctx context.Context, key string, value any, ttl time.Du
 	return r.redisClient.Set(ctx, key, value, ttl).Err()
 }
 
-func (r *RedisStore) Incr(ctx context.Context, key string) error {
-	return r.redisClient.Incr(ctx, key).Err()
+func (r *RedisStore) Incr(ctx context.Context, key string) (int64, error) {
+	return r.redisClient.Incr(ctx, key).Result()
 }
 
 func (r *RedisStore) Get(ctx context.Context, key string) ([]byte, error) {
-	return r.redisClient.Get(ctx, key).Bytes()
+	res, err := r.redisClient.Get(ctx, key).Bytes()
+	switch {
+	case errors.Is(err, redis.Nil):
+		return nil, ErrKeyNotFound
+	case err != nil:
+		return nil, err
+	}
+	return res, nil
 }
 
 func (r *RedisStore) Del(ctx context.Context, key string) error {
 	return r.redisClient.Del(ctx, key).Err()
+}
+
+func (r *RedisStore) Expire(ctx context.Context, key string, ttl time.Duration) error {
+	return r.redisClient.Expire(ctx, key, ttl).Err()
 }
